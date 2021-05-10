ed72950fe25bd07a88edcb1f00fa14595980158a
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-05-10 17:42:00 +0200

    Adding one city dynamically implemented

--- app/src/main/java/com/klima7/astroweather/PlaceActivity.java
+++ app/src/main/java/com/klima7/astroweather/PlaceActivity.java
@@ -5,6 +5,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.klima7.astroweather.yahoo.YahooLocation;
+import com.klima7.astroweather.yahoo.YahooLocationRequest;
 
 import java.util.ArrayList;
 
@@ -20,5 +26,20 @@ public class PlaceActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recycler.setAdapter(adapter);
         recycler.setLayoutManager(layoutManager);
+
+        RequestManager requestManager = RequestManager.getInstance(this);
+        YahooLocationRequest reques = new YahooLocationRequest("Kompina", new Response.Listener<YahooLocation>() {
+            @Override
+            public void onResponse(YahooLocation location) {
+                Log.i("Hello", "location = " + location);
+                adapter.addLocation(location);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Hello", "Error response received");
+            }
+        });
+        requestManager.addToRequestQueue(reques);
     }
 }
